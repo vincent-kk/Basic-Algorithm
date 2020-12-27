23bf883c9eb2d31a9589bb21ace2950a943f3fcd
Lumy Kelvin <lunox273@gmail.com>
2020-12-27 14:46:08 +0900

    yosef

--- 1. Data Structure/queue.py
+++ 1. Data Structure/queue.py
@@ -4,21 +4,21 @@ import collections
 
 input = sys.stdin.readline
 
-s = collections.deque([])
+q = collections.deque([])
 n = int(input())
 
 for _ in range(n):
     raw = input()[:-1]
     if raw == "pop":
-        print(s.popleft() if (len(s) > 0) else -1)
+        print(q.popleft() if (len(q) > 0) else -1)
     elif raw == "front":
-        print(s[0] if (len(s) > 0) else -1)
+        print(q[0] if (len(q) > 0) else -1)
     elif raw == "back":
-        print(s[-1] if (len(s) > 0) else -1)
+        print(q[-1] if (len(q) > 0) else -1)
     elif raw == "size":
-        print(len(s))
+        print(len(q))
     elif raw == "empty":
-        print(0 if len(s) > 0 else 1)
+        print(0 if len(q) > 0 else 1)
     else:
         op, arg = raw.split()
-        s.append(int(arg))
+        q.append(int(arg))
---
+++ 1. Data Structure/yosef.py
@@ -0,0 +1,22 @@
+
+import collections
+import sys
+
+input = sys.stdin.readline
+
+raw = input()
+n, step = raw.split()
+
+queue = collections.deque(range(1, int(n)+1))
+result = []
+
+while len(queue) > 0:
+    for _ in range(int(step) - 1):
+        queue.append(queue.popleft())
+    result.append(queue.popleft())
+
+print('<', end='')
+for n in result[:-1]:
+    print(n, end=', ')
+print(result[-1], end='')
+print('>')

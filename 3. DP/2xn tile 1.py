7bdf3044482a39eeb8885e454f1cb49ecc859a9f
Lumy Kelvin <lunox273@gmail.com>
2020-12-31 20:59:07 +0900

    no. 11726

---
+++ 3. DP/2xn tile 1.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+if n < 2:
+    print(n)
+    exit(0)
+d = [0] * (n+1)
+d[1] = 1
+d[2] = 2
+
+for i in range(n+1):
+    if i < 3:
+        continue
+    d[i] = (d[i-1] % 10007 + d[i-2] % 10007) % 10007
+
+print(d[n])

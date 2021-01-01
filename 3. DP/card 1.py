49395dc713c5f4a9c8a6ef81b1fd1bef4c4853d0
Lumy Kelvin <lunox273@gmail.com>
2021-01-01 22:34:15 +0900

    no. 11052

---
+++ 3. DP/card 1.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def run(n, p):
+    if n == 0:
+        return 0
+    d = [0] * (n+1)
+    p = [0] + p
+    for i in range(1, n+1):
+        for j in range(1, i+1 if (i+1 < len(p))else len(p)):
+            if d[i] == 0 or d[i-j]+p[j] > d[i]:
+                d[i] = d[i-j]+p[j]
+    return d[n]
+
+
+n = int(input())
+p = list(map(int, input().split()))
+
+print(run(n, p))

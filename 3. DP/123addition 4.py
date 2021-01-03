280ac8a1c487d16a8ba1b82ac668941fd27bb0e6
Lumy Kelvin <lunox273@gmail.com>
2021-01-03 21:51:47 +0900

    no. 15989

--- 3. DP/123addition 4.py
+++ 3. DP/123addition 4.py
@@ -1,17 +1,33 @@
-from sys import stdin
+import sys
 
-input = stdin.readline
+input = sys.stdin.readline
 
-dp = [0] * 1000001
-dp[0] = dp[1] = 1
-dp[2] = 2
-dp[3] = 4
 
-for i in range(4, 1000001):
-    dp[i] = ((dp[i-1] % 1000000009)+(dp[i-2] % 1000000009) +
-             (dp[i-3] % 1000000009)) % 1000000009
+def run(n):
+    if n == 0:
+        return(0)
+    dp = [[0]*3 for _ in range(n+1)]
+    for i in range(1, n+1):
+        for j in range(1, 4):
+            if i-j < 0:
+                continue
+            elif i-j == 0:
+                dp[i][j-1] = 1
+            else:
+                if j == 1:
+                    dp[i][j-1] = 0 if (i < 1) else (dp[i - 1][0])
+                elif j == 2:
+                    dp[i][j-1] = 0 if (i < 2) else (dp[i - 2][0] + dp[i-2][1])
+                elif j == 3:
+                    dp[i][j-1] = 0 if (i < 3) else (dp[i - 3]
+                                                    [0] + dp[i-3][1]+dp[i-3][2])
+                else:
+                    pass
+    return dp
+
 
 t = int(input())
-for _ in range(t):
+d = run(10001)
+for i in range(t):
     n = int(input())
-    print(dp[n])
+    print(sum(d[n]))

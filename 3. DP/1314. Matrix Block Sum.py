73ed8af92ff13ce8482a072e7332bda6e4d68839
Lumy Kelvin <lunox273@gmail.com>
2021-01-10 21:55:45 +0900

    1314. first

---
+++ 3. DP/1314. Matrix Block Sum.py
@@ -0,0 +1,48 @@
+from typing import List
+
+
+# 33% Faster than Python's Built-in MAX Function
+def fmax(x, y): return x if x > y else y
+# Idem for MIN Function
+def fmin(x, y): return x if x < y else y
+def inizero(r, c): return [[0]*c for _ in range(r)]
+
+
+class Solution:
+    def matrixBlockSum(self, mat: List[List[int]], K: int):
+        row, column = len(mat), len(mat[0])
+
+        answer: List[List[int]] = [[0]*column for _ in range(row)]
+        for i in range(K+1 if K+1 < row else row):
+            answer[0][0] += sum(mat[i][0:K+1 if K+1 < column else column])
+
+        for r in range(row):
+            for c in range(column):
+                if r == 0 and c == 0:
+                    continue
+                if c == 0:
+                    end = column if c+K+1 > column else c+K+1
+                    answer[r][c] = answer[r-1][c] - \
+                        (0 if r-K-1 < 0 else sum(mat[r-K-1][0:end])) + \
+                        (0 if r+K > row - 1 else sum(mat[r+K][0:end]))
+                else:
+                    start = 0 if r-K < 0 else r-K
+                    end = row if r+K+1 > row else r+K+1
+
+                    leave = 0
+                    arrive = 0
+
+                    if c-K-1 >= 0:
+                        for i in range(start, end):
+                            leave += mat[i][c-K-1]
+                    if c+K < column:
+                        for i in range(start, end):
+                            arrive += mat[i][c+K]
+
+                    answer[r][c] = answer[r][c-1] - leave + arrive
+        return answer
+
+
+s = Solution()
+mat: List[List[int]] = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+s.matrixBlockSum(mat, 2)

5f17090da3f5e3ed52b69bf62997667139a99f9b
Lumy Kelvin <lunox273@gmail.com>
2021-02-15 22:24:39 +0900

    change formatter

--- 3. DP/877. Stone Game.py
+++ 3. DP/877. Stone Game.py
@@ -4,7 +4,7 @@ from typing import List
 class Solution:
     def stoneGame(self, piles: List[int]):
         length = len(piles)
-        memo = [[-1]*length for _ in range(length)]
+        memo = [[-1] * length for _ in range(length)]
 
         def dp(start, end, player):
             if start == end:
@@ -19,12 +19,12 @@ class Solution:
             if memo[start][end] != -1:
                 return memo[start][end]
 
-            first = piles[start]+dp(start+1, end, player+1)
-            second = piles[end]+dp(start, end-1, player+1)
+            first = piles[start] + dp(start + 1, end, player + 1)
+            second = piles[end] + dp(start, end - 1, player + 1)
             memo[start][end] = max(first, second)
             return memo[start][end]
 
-        return dp(0, length-1, 0) > sum(piles)//2
+        return dp(0, length - 1, 0) > sum(piles) // 2
 
 
 s = Solution()

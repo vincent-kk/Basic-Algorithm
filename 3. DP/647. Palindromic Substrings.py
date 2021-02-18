eb8e3bd347da0e01564f422b6a89f3fa7c034422
Lumy Kelvin <lunox273@gmail.com>
2021-02-18 23:43:05 +0900

    Runtime: 180 ms, faster than 58.61% of Python3

---
+++ 3. DP/647. Palindromic Substrings.py
@@ -0,0 +1,34 @@
+from functools import cache
+
+
+class Solution:
+    def countSubstrings(self, s: str):
+        length = len(s) - 1
+        conter = 0
+
+        @cache
+        def compair(i, j):
+            if i < 0 or i > length:
+                return False
+            if j < 0 or j > length:
+                return False
+            return s[i] == s[j]
+
+        for i in range(length + 1):
+
+            size = 0
+            while compair(i - size, i + size):
+                conter += 1
+                size += 1
+
+            size = 0
+            while compair(i - size, i + 1 + size):
+                conter += 1
+                size += 1
+        return conter
+
+
+s = Solution()
+i = "abc"
+o = s.countSubstrings(i)
+print(o)

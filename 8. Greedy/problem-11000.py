11ec8545d3a62e74ab64d9adc7c46fc037cb8281
Lumy Kelvin <lunox273@gmail.com>
2021-02-25 00:24:13 +0900

    11000 : use pq, 1072ms

--- 8. Greedy/problem-11000.py
+++ 8. Greedy/problem-11000.py
@@ -1,33 +1,56 @@
 import sys
+
+# import heapq
+from queue import PriorityQueue
 from operator import itemgetter
 
 input = sys.stdin.readline
 
-num = int(input())
-classes = []
+N = int(input())
 
-for _ in range(num):
-    start, end = map(int, input().split())
-    classes.append((start, end))
-classes.sort(key=itemgetter(0))
+timeTable = [list(map(int, input().split())) for _ in range(N)]
+timeTable.sort(key=itemgetter(0))
 
-num_of_classroom = 0
-closest_end = []
-while len(classes) > 0:
-    start, end = classes.pop(0)
-    if len(closest_end) == 0 or closest_end[0] > start:
-        num_of_classroom += 1
+classrooms = PriorityQueue()
+classrooms.put(timeTable[0][1])
+# heapq.heappush(classrooms, timeTable[0][1])
+
+for start, end in timeTable[1:]:
+    if classrooms.queue[0] > start:
+        classrooms.put(end)
+        # heapq.heappush(classrooms, end)
     else:
-        closest_end.pop()
+        classrooms.get()
+        classrooms.put(end)
+        # heapq.heappop(classrooms)
+        # heapq.heappush(classrooms, end)
 
-    closest_end.append(end)
-    closest_end.sort()
+print(len(classrooms.queue))
 
-    # index = 0
-    # while index < len(classes):
-    #     if end <= classes[index][0]:
-    #         start, end = classes.pop(index)
-    #         index -= 1
-    #     index += 1
+# classes = []
 
-print(num_of_classroom)
+# for _ in range(num):
+#     start, end = map(int, input().split())
+#     classes.append((start, end))
+# classes.sort(key=itemgetter(0))
+
+# num_of_classroom = 0
+# closest_end = []
+# while len(classes) > 0:
+#     start, end = classes.pop(0)
+#     if len(closest_end) == 0 or closest_end[0] > start:
+#         num_of_classroom += 1
+#     else:
+#         closest_end.pop()
+
+#     closest_end.append(end)
+#     closest_end.sort()
+
+# index = 0
+# while index < len(classes):
+#     if end <= classes[index][0]:
+#         start, end = classes.pop(index)
+#         index -= 1
+#     index += 1
+
+# print(num_of_classroom)

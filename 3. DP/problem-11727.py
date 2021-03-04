9d32b1e5584cd80a488118bb84942fe11c0070a1
Lumy Kelvin <lunox273@gmail.com>
2021-03-04 16:30:36 +0900

    11727. 72ms, use fibonacci

---
+++ 3. DP/problem-11727.py
@@ -0,0 +1,29 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def countTile(N: int):
+    end_with_tile1x2 = 1
+    end_with_tile2x1 = 1
+    end_with_tile2x2 = 1
+    if N < 2:
+        return end_with_tile1x2
+    for _ in range(N - 2):
+        end_with_tile1x2, end_with_tile2x1, end_with_tile2x2 = (
+            (
+                end_with_tile1x2 % 10007
+                + end_with_tile2x1 % 10007
+                + end_with_tile2x2 % 10007
+            )
+            % 10007,
+            end_with_tile1x2,
+            end_with_tile1x2,
+        )
+    return (
+        end_with_tile1x2 % 10007 + end_with_tile2x1 % 10007 + end_with_tile2x2 % 10007
+    ) % 10007
+
+
+i = int(input())
+print(countTile(i))

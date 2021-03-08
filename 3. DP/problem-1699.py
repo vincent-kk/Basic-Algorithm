be0dcbb18137779e6d0b5fe5755b92a87930c408
Lumy Kelvin <lunox273@gmail.com>
2021-03-08 16:14:22 +0900

    5236ms

--- 3. DP/problem-1699.py
+++ 3. DP/problem-1699.py
@@ -1,50 +1,38 @@
 import sys
-from functools import cache
+import collections
 
 input = sys.stdin.readline
 
 
 def sumOfPoweredNums(N: int):
-    # @cache
-    # def findPoweredNum(n: int):
-    #     if n == 0:
-    #         return 0
-    #     if n == 1:
-    #         return 1
-    #     i = 1
-    #     local_minimum = float("inf")
-    #     while i * i < n:
-    #         local_minimum = min(local_minimum, findPoweredNum(n - (i * i)))
-    #         i += 1
-    #     return local_minimum + 1
-
-    # return findPoweredNum(N)
-
-    cache = [0] * (N + 1)
+    cache = collections.defaultdict(int)
     for n in range(1, N + 1):
         i = 1
         cache[n] = n
-        while i * i < n:
-            cache[n] = min(cache[n], cache[n - (i * i)] + 1)
+        while True:
+            if i * i > n:
+                break
+            if cache[n] > cache[n - (i * i)] + 1:
+                cache[n] = cache[n - (i * i)] + 1
             i += 1
     return cache[N]
 
 
-# N = int(input())
-# print(sumOfPoweredNums(N))
+N = int(input())
+print(sumOfPoweredNums(N))
 
 
 # import sys
 
-N = int(sys.stdin.readline())
+# N = int(sys.stdin.readline())
 
-dp = [0] * 100001
+# dp = [0] * 100001
 
-for i in range(1, N + 1):
-    dp[i] = i
-    for j in range(1, i):
-        if (j * j) > i:
-            break
-        if dp[i] > dp[i - j * j] + 1:
-            dp[i] = dp[i - j * j] + 1
-print(dp[N])
+# for i in range(1, N + 1):
+#     dp[i] = i
+#     for j in range(1, i):
+#         if (j * j) > i:
+#             break
+#         if dp[i] > dp[i - j * j] + 1:
+#             dp[i] = dp[i - j * j] + 1
+# print(dp[N])

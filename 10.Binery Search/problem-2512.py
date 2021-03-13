d086b58b65be9e8db565088075e8f4238b8220eb
Lumy Kelvin <lunox273@gmail.com>
2021-03-13 15:55:44 +0900

    2512. 144ms

--- 10.Binery Search/problem-2512.py
+++ 10.Binery Search/problem-2512.py
@@ -4,5 +4,30 @@ from typing import List
 input = sys.stdin.readline
 
 
-def solution(costs):
-    pass
+def solution(M: int, costs: List[int]):
+
+    low, high = 0, max(costs)
+    if sum(costs) <= M:
+        return high
+
+    while high > low + 1:
+        pivot = (low + high) // 2
+        total = 0
+        for cost in costs:
+            total += cost if cost < pivot else pivot
+
+        if total < M:
+            low = pivot
+        elif total > M:
+            high = pivot
+        else:
+            break
+
+    return (low + high) // 2
+
+
+N = int(input())
+costs = list(map(int, input().split()))
+M = int(input())
+
+print(solution(M, costs))

0849e0c461bdbba543d04c7cb61d899afe45c579
Lumy Kelvin <lunox273@gmail.com>
2021-03-15 20:17:30 +0900

    24 ms, faster than 95.20% of Python3

--- 3. DP/70. Climbing Stairs.py
+++ 3. DP/70. Climbing Stairs.py
@@ -1,19 +1,32 @@
 from functools import cache
+from collections import defaultdict
 
 
 class Solution:
-    def climbStairs(self, n: int):
-        @cache
-        def makeSteps(x: int):
-            if x == 0:
-                return 0
-            if x == 1:
-                return 1
-            if x == 2:
-                return 2
-            return makeSteps(x - 1) + makeSteps(x - 2)
+    # Top-Down
+    # def climbStairs(self, n: int):
+    #     @cache
+    #     def makeSteps(x: int):
+    #         if x == 0:
+    #             return 0
+    #         if x == 1:
+    #             return 1
+    #         if x == 2:
+    #             return 2
+    #         return makeSteps(x - 1) + makeSteps(x - 2)
 
-        return makeSteps(n)
+    #     return makeSteps(n)
+
+    # Buttom-Up
+    def climbStairs(self, n: int):
+        cache = defaultdict(int)
+        cache[1] = 1
+        cache[2] = 2
+
+        for i in range(3, n + 1):
+            cache[i] = cache[i - 1] + cache[i - 2]
+
+        return cache[n]
 
 
 if __name__ == "__main__":

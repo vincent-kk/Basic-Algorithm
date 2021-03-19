8272cba9197c95f155c9349426ad19b87f088d3b
Lumy Kelvin <lunox273@gmail.com>
2021-03-19 12:11:25 +0900

    15732. 192ms

--- 10.Binery Search/problem-15732.py
+++ 10.Binery Search/problem-15732.py
@@ -5,7 +5,23 @@ input = sys.stdin.readline
 
 
 def solution(N: int, K: int, D: int, rules: List[int]):
-    return rules
+    low, high = 1, N
+    while high > low:
+        pivot = (low + high) // 2
+        acorn = D
+        for start, end, step in rules:
+            if start > pivot:
+                continue
+            end = end if end < pivot else pivot
+            need = ((end - start) // step) + 1
+            acorn -= need
+            if acorn < 0:
+                break
+        if acorn > 0:
+            low = pivot + 1
+        else:
+            high = pivot
+    return (low + high) // 2
 
 
 if __name__ == "__main__":

56aa9f1b989e752b3223a1449e65cd21ad1d7a14
Lumy Kelvin <lunox273@gmail.com>
2021-04-07 13:16:30 +0900

    ok

--- programmers/lv2/prefix.py
+++ programmers/lv2/prefix.py
@@ -1,6 +1,13 @@
 def solution(phone_book):
-    answer = True
-    return answer
+    sorted_phone_book = sorted(phone_book)
+    prefix = None
+    for phone in sorted_phone_book:
+        if prefix and phone.startswith(prefix):
+            return False
+        else:
+            prefix = phone
+
+    return True
 
 
 if __name__ == "__main__":

e7dba26ed489bc6d4ad4527e86d8135bde7fb33d
Lumy Kelvin <lunox273@gmail.com>
2021-04-13 16:44:17 +0900

    35. ok

---
+++ programmers/lv1/12943.py
@@ -0,0 +1,18 @@
+def solution(num: int):
+
+    answer = 0
+    while num != 1:
+        if num % 2 == 0:
+            num = num // 2
+        else:
+            num = num * 3 + 1
+        answer += 1
+        if answer > 500:
+            return -1
+
+    return answer
+
+
+if __name__ == "__main__":
+    i = 626331
+    print(solution(i))

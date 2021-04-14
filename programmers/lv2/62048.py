1442bdbde98eef0653a9e815d561a20e05a68c2b
Lumy Kelvin <lunox273@gmail.com>
2021-04-14 15:24:56 +0900

    add

--- programmers/lv2/62048.py
+++ programmers/lv2/62048.py
@@ -2,6 +2,8 @@ from math import gcd
 
 
 def solution(w: int, h: int):
+
+    # 계산수를 줄인 구분구적법 사용 O(w/gcd)
     # g = gcd(w, h)
     # dw = w // g
     # dh = h // g
@@ -12,11 +14,13 @@ def solution(w: int, h: int):
 
     # return w * h - s * g
 
+    # 구분구적법을 사용한 방법 O(w)
     # answer = 0
     # for i in range(1, w):
     #     answer += int(i * h / w)
     # return answer * 2
 
+    # 씨발년들
     return w * h - (w + h - gcd(w, h))
 
 

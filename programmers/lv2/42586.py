68a49f336b38ba5dec10a698fb80aa6021af4dc1
Lumy Kelvin <lunox273@gmail.com>
2021-04-15 17:17:21 +0900

    3. need to study

--- programmers/lv2/42586.py
+++ programmers/lv2/42586.py
@@ -25,23 +25,34 @@ def solution(progresses: List[int], speeds: List[int]) -> List[int]:
             projects[i] += speeds[i]
             if projects[i] > 99:
                 done.append(i)
+
+        done.sort(reverse=True)
         for i in done:
-            if i > release_turn:
-                heapq.heappush(delay, i)
-            else:
-                r = 1
-                release_turn += 1
+            heapq.heappush(delay, i)
+            if i <= release_turn:
+                r = 0
                 while len(delay) > 0 and delay[0] <= release_turn:
+                    heapq.heappop(delay)
                     release_turn += 1
                     r += 1
-                    heapq.heappop(delay)
                 release_queue.append(r)
             del projects[i]
 
     return release_queue
 
+    """
+    def solution(progresses, speeds):
+    Q=[]
+    for p, s in zip(progresses, speeds):
+        if len(Q)==0 or Q[-1][0]<-((p-100)//s):
+            Q.append([-((p-100)//s),1])
+        else:
+            Q[-1][1]+=1
+    return [q[1] for q in Q]
+    """
+
 
 if __name__ == "__main__":
-    p = [95, 90, 99, 99, 80, 99]
+    p = [99, 99, 99, 98, 98, 99]
     s = [1, 1, 1, 1, 1, 1]
     print(solution(p, s))

293106d7b60aec8261d3a7bbc5f3c78a03ab1630
Lumy Kelvin <lunox273@gmail.com>
2021-04-16 17:28:48 +0900

    pass

--- programmers/lv2/12899.py
+++ programmers/lv2/12899.py
@@ -1,13 +1,16 @@
+from collections import deque
+
+
 def solution(n: int) -> str:
-    stack = []
+    dq = deque()
     n -= 1
     while n >= 3:
-        stack.append(str(2 ** (n % 3)))
+        dq.appendleft(str(2 ** (n % 3)))
         n = (n // 3) - 1
 
-    stack.append(str(2 ** (n % 3)))
+    dq.appendleft(str(2 ** (n % 3)))
 
-    return "".join(reversed(stack))
+    return "".join(dq)
 
 
 if __name__ == "__main__":

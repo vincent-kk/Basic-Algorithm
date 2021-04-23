5d6ff1dd2f26d77ec7c9fcfb99fdc46e1af39e26
Lumy Kelvin <lunox273@gmail.com>
2021-04-23 17:04:34 +0900

    41. need to use Dijkstra

--- programmers/lv2/12978.py
+++ programmers/lv2/12978.py
@@ -1,37 +1,44 @@
 from collections import defaultdict
+import heapq
 
 
 def solution(N, road, K):
-    edges = defaultdict(list)
-    costs = defaultdict(lambda: 10001)
+    edges = defaultdict(dict)
     for s, d, c in road:
-        edges[s].append(d)
-        edges[d].append(s)
+        if d in edges[s] and s in edges[d]:
+            edges[s][d] = min(c, edges[s][d])
+            edges[d][s] = min(c, edges[d][s])
+        else:
+            edges[s][d] = c
+            edges[d][s] = c
 
-        key = f"{s}-{d}" if s < d else f"{d}-{s}"
-        costs[key] = min(costs[key], c)
-
+    distances = defaultdict(lambda: K + 1)
     start = 1
-    stack = [(start, 0)]
-    visited = set([start])
+    distances[start] = 0
+    queue = []
+    heapq.heappush(queue, [distances[start], start])
 
-    while stack:
-        this, cost = stack[-1]
-        pop = True
+    while queue:  # queue에 남아 있는 노드가 없으면 끝
+        current_distance, current_destination = heapq.heappop(
+            queue
+        )  # 탐색 할 노드, 거리를 가져옴.
 
-        for n in edges[this]:
-            if n in visited:
+        if (
+            distances[current_destination] < current_distance
+        ):  # 기존에 있는 거리보다 길다면, 볼 필요도 없음
+            continue
+
+        for new_destination, new_distance in edges[current_destination].items():
+            distance = current_distance + new_distance  # 해당 노드를 거쳐 갈 때 거리
+            if distance > K:
                 continue
-            c = costs[f"{this}-{n}" if this < n else f"{n}-{this}"]
-            if cost + c > K:
-                continue
-            visited.add(n)
-            stack.append((n, cost + c))
-            pop = False
-        if pop:
-            stack.pop()
+            if distance < distances[new_destination]:  # 알고 있는 거리 보다 작으면 갱신
+                distances[new_destination] = distance
+                heapq.heappush(
+                    queue, [distance, new_destination]
+                )  # 다음 인접 거리를 계산 하기 위해 큐에 삽입
 
-    return len(visited)
+    return len(distances)
 
 
 if __name__ == "__main__":

c0b2e8963712b34dbaae7a0da00fef567f50c921
Lumy Kelvin <lunox273@gmail.com>
2021-05-07 21:28:06 +0900

    need to study

---
+++ 14. Back Tracking/401. Binary Watch.py
@@ -0,0 +1,26 @@
+class Solution:
+    _LED_NUM = 10
+    _HOUR_LED_NUM = 4
+
+    # Backtracking
+    def __init__(self):
+        self._times = []
+
+    def readBinaryWatch(self, num: int):
+        self._backtrack(num, 0, 0, 0)
+        return self._times
+
+    def _backtrack(self, num: int, pos: int, hour: int, minute: int):
+        if hour > 11 or minute > 59:
+            return
+        elif num == 0:
+            self._times.append("{:d}:{:02d}".format(hour, minute))
+            return
+
+        for i in range(pos, Solution._LED_NUM):
+            if i < Solution._HOUR_LED_NUM:
+                self._backtrack(num - 1, i + 1, hour + 2 ** i, minute)
+            else:
+                self._backtrack(
+                    num - 1, i + 1, hour, minute + 2 ** (i - Solution._HOUR_LED_NUM)
+                )

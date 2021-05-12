2dbb8b3d86d32a214cbd7838fc82b6e3f0b3a018
Lumy Kelvin <lunox273@gmail.com>
2021-05-12 19:38:14 +0900

    3. better solution

--- programmers/lv3/43238.py
+++ programmers/lv3/43238.py
@@ -2,22 +2,40 @@ from typing import List
 
 
 def solution(n: int, times: List[int]):
-    left, right = 1, n * max(times)
-    answer = 0
-    while left < right - 1:
-        pivot = (right + left) // 2
+    left, right = n, max(times) * n
+    while left < right:
+        pivot = (left + right) // 2
+
         people = 0
         for time in times:
             people += pivot // time
             if people >= n:
                 break
+
         if people < n:
             left = pivot + 1
         else:
-            answer = pivot
-            right = pivot - 1
+            right = pivot
+    return left
 
-    return answer
+
+# def solution(n: int, times: List[int]):
+#     left, right = 1, n * max(times)
+#     answer = 0
+#     while left < right - 1:
+#         pivot = (right + left) // 2
+#         people = 0
+#         for time in times:
+#             people += pivot // time
+#             if people >= n:
+#                 break
+#         if people < n:
+#             left = pivot + 1
+#         else:
+#             answer = pivot
+#             right = pivot - 1
+
+#     return answer
 
 
 if __name__ == "__main__":

d53fb9827a7e1e6f86b703206010fa3d874d67b5
KLumy <lunox273@gmail.com>
2021-05-13 14:10:26 +0900

    8. resolved needed

--- programmers/lv3/49191.py
+++ programmers/lv3/49191.py
@@ -15,23 +15,27 @@ def solution(n: int, results: List[List[int]]) -> int:
     Returns:
         int: 정확하게 순위를 매길 수 있는 선수의 수
     """
-
+    # key가 이긴 상대, key가 진 상대
     winners, losers = defaultdict(set), defaultdict(set)
 
     for i in range(1, n + 1):
+        # 내가 이겼거나 진 상대를 나의 승리/패배 목록에 추가한다
         for winner, loser in results:
             if winner == i:
                 winners[i].add(loser)
             if loser == i:
                 losers[i].add(winner)
-        for winner in losers[i]:
-            winners[winner].update(winners[i])
-        for loser in winners[i]:
-            losers[loser].update(losers[i])
+        # 내가 진 모든 상대의 승리목록에 나의 승리 목록을 추가한다
+        for w in losers[i]:
+            winners[w].update(winners[i])
+        # 내가 이긴 모든 상대의 패배목록에 나에게 진 사람들을 추가한다
+        for l in winners[i]:
+            losers[l].update(losers[i])
 
     count = 0
 
     for i in range(1, n + 1):
+        # 내가 이긴 사람의 수와 진 사람의 수가 나를 제외한 전체 사람의 수와 동일하다면 순위를 알 수 있다
         if len(winners[i]) + len(losers[i]) == n - 1:
             count += 1
 
